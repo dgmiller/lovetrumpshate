d4785e1dbfd36292a357d0103c5e2f073cc7d027
derekgm <derekgm@byu.local@acme05.byu.edu>
2017-04-20 00:05:08 -0600

    crazy updates but it is finished

--- capstone/submission/dtrumpimg.py
+++ capstone/submission/dtrumpimg.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-fname = 'me'
-cmap = 'winter'
+fname = 'thumbsup'
+cmap = 'gist_stern'
 t1 = mpimg.imread('t'+fname+'.jpg')
 fig = plt.figure(frameon=False)
 ax = plt.Axes(fig,[0.,0.,1.,1.])
 ax.set_axis_off()
 fig.add_axes(ax)
 ax.imshow(t1[:,:,0],cmap=cmap)
-fig.savefig('T'+fname+'.png',bbox_inches='tight',pad_inches=0)
+fig.savefig('T2'+fname+'.png',bbox_inches='tight',pad_inches=0)
